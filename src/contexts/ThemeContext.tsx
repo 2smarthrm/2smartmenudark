55f4bf8403e5bf7f967591aa8e2e3b86bcf78874
2smarthrm <2smarthrm@gmail.com>
2025-09-10 10:54:19 +0100

    Removed 3 reports
    Registo de Tempos de Trabalho, 
    Marcações Diárias,
    Mapa Resumos e Tempos de Trabalho (Excluindo  Horas Noturnas)

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -35,3 +35,41 @@ export const ThemeProvider: React.FC<React.PropsWithChildren> = ({
     </ThemeContext.Provider>
   );
 };
+
+// WHITEEEEEEEEEE
+// src/contexts/ThemeContext.tsx
+// import React, { createContext, useContext, useEffect, useState } from "react";
+
+// type ThemeContextType = {
+//   isDark: boolean;
+//   toggleTheme: () => void; // no-op
+// };
+
+// const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+// export const useTheme = () => {
+//   const ctx = useContext(ThemeContext);
+//   if (!ctx) throw new Error("useTheme must be used within a ThemeProvider");
+//   return ctx;
+// };
+
+// export const ThemeProvider: React.FC<React.PropsWithChildren> = ({
+//   children,
+// }) => {
+//   // Força light SEMPRE
+//   const [isDark] = useState<boolean>(false);
+
+//   useEffect(() => {
+//     const root = document.documentElement;
+//     root.classList.add("light");
+//     root.classList.remove("dark");
+//   }, []);
+
+//   const toggleTheme = () => {}; // desativado
+
+//   return (
+//     <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// };
